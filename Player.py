a7d189f0c51d9b49c95581602e7b6ecbc6706a9e
djeof-1 <shreyasiyer14@gmail.com>
2015-12-01 11:59:55 +0530

    3rd commit

--- Player.py
+++ Player.py
@@ -20,13 +20,13 @@ class Player:
     def detectCollisions(self, x1,y1,w1,h1,x2,y2,w2,h2):
        
         if (x2+w2>=x1>=x2 and y2+h2>=y1>=y2):
-            return True
+            return True,1
 
         elif (x2+w2>=x1+w1>=x2 and y2+h2>=y1>=y2):
-            return True
+            return True,1
                     
         elif (x2+w2>=x1>=x2 and y2+h2>=y1+h1>=y2):
-            return True
+            return True,1
 
         elif (x2+w2>=x1+w1>=x2 and y2+h2>=y1+h1>=y2):
             return True
